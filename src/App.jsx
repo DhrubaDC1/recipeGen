29f1429434a1381678c6b6e1bcbbd7bc498841d8
Dhruba Chakraborty <dhruba.chakraborty.2000@gmail.com>
2025-02-27 23:24:51 +0600

    ui: ui mapping

--- src/App.jsx
+++ src/App.jsx
@@ -4,31 +4,31 @@ function removeBackticksAndParse(str) {
   let recipe = JSON.parse(str.replace(/^```json|```$/g, "").trim());
   return recipe;
 }
-async function getRecipe(image, setRecipe) {
-  console.log("🚀 ~ getRecipe ~ image:", image);
-  const formData = new FormData();
-  formData.append("image", image);
-
-  setRecipe("Loading...");
-  const response = await fetch("http://localhost:3000/api/upload", {
-    method: "POST",
-    body: formData,
-  });
-  const data = await response.json();
-  const recipe = removeBackticksAndParse(data.recipe);
-
-  console.log("🚀 ~ getRecipe ~ recipe:", recipe);
-  setRecipe(recipe);
-}
 
 function App() {
-  const [recipe, setRecipe] = useState("");
+  const [recipe, setRecipe] = useState("Loading...");
   const [image, setImage] = useState(null);
+  async function getRecipe(image) {
+    console.log("🚀 ~ getRecipe ~ image:", image);
+    const formData = new FormData();
+    formData.append("image", image);
+
+    setRecipe("Loading...");
+    const response = await fetch("http://localhost:3000/api/upload", {
+      method: "POST",
+      body: formData,
+    });
+    const data = await response.json();
+    const recipe = removeBackticksAndParse(data.recipe);
+
+    console.log("🚀 ~ getRecipe ~ recipe:", recipe);
+    setRecipe(recipe);
+  }
 
   const handleImageUpload = (event) => {
     const file = event.target.files[0];
     setImage(file);
-    getRecipe(file, setRecipe);
+    getRecipe(file);
   };
 
   const handleTakePhoto = async () => {
@@ -46,7 +46,7 @@ function App() {
       context.drawImage(video, 0, 0, canvas.width, canvas.height);
       canvas.toBlob((blob) => {
         setImage(blob);
-        getRecipe(blob, setRecipe);
+        getRecipe(blob);
         stream.getTracks().forEach((track) => track.stop());
       }, "image/png");
     });
@@ -54,13 +54,68 @@ function App() {
 
   return (
     <>
-      <div>
-        <h1>Recipe Generator</h1>
+      <div style={{ textAlign: "start" }}>
         <div>
-          <input type="file" accept="image/*" onChange={handleImageUpload} />
-          <button onClick={handleTakePhoto}>Take Photo</button>
+          {recipe !== "Loading..." ? (
+            <div>
+              {image && (
+                <img
+                  src={URL.createObjectURL(image)}
+                  alt="Uploaded"
+                  style={{ maxWidth: "100%", height: "auto" }}
+                />
+              )}
+              <h2>Recipe for {recipe?.recipe_name}</h2>
+              <h2>Image Description</h2>
+              <p>{recipe?.image_description}</p>
+              <h2>Ingredients</h2>
+              <ul>
+                {recipe?.ingredients.map((ingredient) => (
+                  <li key={ingredient.item_name}>
+                    {ingredient.item_name} - {ingredient.quantity}
+                  </li>
+                ))}
+              </ul>
+              <h2>Instructions</h2>
+              <ol>
+                {recipe?.instructions.map((instruction, index) => (
+                  <li key={index}>{instruction}</li>
+                ))}
+              </ol>
+              <h2>Nutrition</h2>
+              <ul>
+                {Object.entries(recipe?.nutrition_count).map(([key, value]) => (
+                  <li key={key}>
+                    {key}: {value}
+                  </li>
+                ))}
+              </ul>
+              <div>
+                <p>Upload Another Image</p>
+                <input
+                  type="file"
+                  accept="image/*"
+                  placeholder="Upload another image"
+                  onChange={handleImageUpload}
+                />
+              </div>
+            </div>
+          ) : (
+            <div>
+              <h1>Recipe Generator</h1>
+              <div>
+                <h2>Upload an image</h2>
+                <input
+                  type="file"
+                  accept="image/*"
+                  placeholder="Upload an image"
+                  onChange={handleImageUpload}
+                />
+              </div>
+              <p>No recipe available. Please upload an image.</p>
+            </div>
+          )}
         </div>
-        <div></div>
       </div>
     </>
   );
--- src/getResponse.js
+++ src/getResponse.js
@@ -54,7 +54,7 @@ export default async function getRecipe(path) {
             },
           },
           {
-            text: "Given this image:\nOnly Provide details in JSON format\n First, describe the image\nThen, detail the recipe to bake this item. Include item names and quantities for the recipe. Only JSON nothing else.",
+            text: "Given this image:\nOnly Provide the recipe details in JSON format\n Describe of the image as image_description, Recipe name field as recipe_name. Give ingredients as array of objects each containing item_name and quantity. Finally give instructions array. Also include the nutrition count for this recipe as nutrition_count. Only JSON nothing else.",
           },
         ],
       },
